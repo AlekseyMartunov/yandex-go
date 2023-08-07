3495d4ff43075996e0c488c484d81a02da056f88
Aleksey Martunov <alekseymartunov@icloud.com>
2023-08-07 21:50:57 +0300

    iter5 is finished

--- internal/app/config.go
+++ internal/app/config.go
@@ -2,18 +2,28 @@ package app
 
 import (
 	"flag"
+	"os"
 )
 
 type netAdress struct {
-	addr     string
-	baseHost string
+	addr     string `env:"SERVER_ADDRESS"`
+	baseHost string `env:"BASE_URL"`
 }
 
 func (a *app) GetConfig() {
+
 	flag.StringVar(&a.cfg.addr, "a", "127.0.0.1:8080", "Адрес для запуска приложения")
 	flag.StringVar(&a.cfg.baseHost, "b", "http://127.0.0.1:8080", "Базовый адрес сокращенного URL")
 
 	flag.Parse()
+
+	if val, ok := os.LookupEnv("SERVER_ADDRESS"); ok {
+		a.cfg.addr = val
+	}
+
+	if val, ok := os.LookupEnv("BASE_URL"); ok {
+		a.cfg.baseHost = val
+	}
 }
 
 func (a *app) GetAdres() string {
